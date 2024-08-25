6512f985b0026215985aef7a3725cab51c85dbf0
mirirosca <roscamiri@gmail.com>
2024-08-25 20:30:19 +0200

    first commit in src folder

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam architecto recusandae maxime minima. Quam quo, ad, numquam itaque ipsa quibusdam voluptatum accusantium dignissimos dicta nobis sit suscipit eveniet consequatur quod?</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
